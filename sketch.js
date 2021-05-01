d0a39df485b5ea50c52060775b605de6f20c6d8f
Shubhansh Khare <shubhanshkharedon99@gmail.com>
2021-05-01 17:53:10 +0530

    added smokeimage

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var bird, slingShot;
+var gameState = "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -68,14 +69,21 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if (gameState!=="launched" ) {
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+    
+   
 }
 
 
 function mouseReleased(){
     slingshot.fly();
-}
+    gameState = "launched";
+} 
 function keyPressed(){
-    if(keyCode===32)
-    slingshot.attach(bird.body);
-}
+    if(keyCode===32) {
+         // slingshot.attach(bird.body);
+    }
+   
+} 
